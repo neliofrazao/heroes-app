cd4d37a5c9c53fab747cfb7b59281ba009a258e1
Nelio Frazao <neliofrazac@gmail.com>
2020-05-07 17:22:45 -0300

    chore: add prettier in app

---
+++ prettier.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  bracketSpacing: true,
+  jsxBracketSameLine: false,
+  printWidth: 100,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  quoteProps: 'preserve',
+};
