12be1de207b5b1b8fe84d40f39d82f73e487c99d
Nelio Frazao <neliofrazac@gmail.com>
2020-05-08 17:46:30 -0300

    test: should render layout component in app.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,10 @@
-test('renders learn react link', () => {
-  expect(true).toBe(true)
+import React from 'react'
+import { render } from '@testing-library/react'
+import App from './App'
+
+describe('App()', () => {
+  test('should render layout component ', () => {
+    const { getByTestId } = render(<App />)
+    expect(getByTestId('data-layout')).toBeDefined()
+  })
 })
