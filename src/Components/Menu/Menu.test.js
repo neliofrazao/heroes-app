0a1697d88f54af16e6e164e9b391c2c4da32d3b3
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 14:14:05 -0300

    test: should render menu component

--- src/Components/Menu/Menu.test.js
+++ src/Components/Menu/Menu.test.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import injectRouter from '../../helpers/testHelpers'
-import MenuItem from './MenuItem'
+import { Menu, MenuItem } from './index'
 
 const BaseRender = () => (
-  <>
+  <Menu>
     <MenuItem link="/hero" title="item menu">
       Home
     </MenuItem>
     <MenuItem link="/busca" title="item menu">
       Busca
     </MenuItem>
-  </>
+  </Menu>
 )
 
 describe('Menu()', () => {
-  test('should render menuItem component ', () => {
+  test('should render menu component ', () => {
+    const { getByTestId } = render(injectRouter(<BaseRender />))
+    expect(getByTestId('data-menu')).toBeDefined()
+  })
+
+  test('should render menuItem component with two items ', () => {
     render(injectRouter(<BaseRender />))
     const menuItems = screen.getAllByRole('button').length
 
