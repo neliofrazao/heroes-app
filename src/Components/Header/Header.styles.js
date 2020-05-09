82812e991c4a24e98bcf82a7c3fb0f9cd1b42d77
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 16:37:55 -0300

    feat: improve header style

--- src/Components/Header/Header.styles.js
+++ src/Components/Header/Header.styles.js
@@ -1,5 +1,6 @@
 import styled from 'styled-components'
 import bgHeader from '../../assets/images/news_articles.jpg'
+import colors from '../../utils/Theme/colors'
 
 const Head = styled.header`
   padding: 3em 0 8em 0;
@@ -7,5 +8,7 @@ const Head = styled.header`
   background-size: cover;
   height: 100%;
   overflow: hidden;
+  border-bottom: 30px solid ${colors.black};
+  border-top: 30px solid ${colors.black};
 `
 export default Head
--- src/utils/Theme/colors.js
+++ src/utils/Theme/colors.js
@@ -2,6 +2,7 @@ const colors = {}
 
 // Background colors
 colors.gray = '#e5e5e5'
+colors.black = '#151515'
 
 // Graphite colors
 colors.graphite = '#383428'
