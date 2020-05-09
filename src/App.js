1260f0cf3baff43a14c290f1ac5f74ac857a194c
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 09:52:20 -0300

    refactor: move layout component to routes file

--- src/App.js
+++ src/App.js
@@ -1,11 +1,6 @@
 import React from 'react'
-import Layout from './utils/Layout'
 import Routes from './Routes'
 
-const App = () => (
-  <Layout>
-    <Routes />
-  </Layout>
-)
+const App = () => <Routes />
 
 export default App
--- src/Routes.js
+++ src/Routes.js
@@ -1,15 +1,18 @@
 import React, { Suspense, lazy } from 'react'
 import { BrowserRouter as Router, Switch, Route, withRouter } from 'react-router-dom'
 import RouteLoader from './utils/RouteLoader'
+import Layout from './utils/Layout'
 
 const HeroList = withRouter(lazy(() => import('./Pages/HeroList')))
 
 const Routes = () => (
   <Router>
     <Suspense fallback={<RouteLoader />}>
-      <Switch>
-        <Route path="/" exact component={HeroList} />
-      </Switch>
+      <Layout>
+        <Switch>
+          <Route path="/" exact component={HeroList} />
+        </Switch>
+      </Layout>
     </Suspense>
   </Router>
 )
