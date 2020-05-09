83e52e10a3ad7d4f35a073e1652e3eda4b634456
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 14:15:21 -0300

    feat: implement  Menu component and shell in Header comonent

--- src/Components/Header/Header.styles.js
+++ src/Components/Header/Header.styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 import bgHeader from '../../assets/images/news_articles.jpg'
 
 const Head = styled.header`
-  padding: 3em 2em 8em 3em;
+  padding: 3em 0 8em 0;
   background: url(${bgHeader}) no-repeat center center fixed;
   background-size: cover;
   height: 100%;
--- src/Components/Header/index.js
+++ src/Components/Header/index.js
@@ -1,10 +1,27 @@
 import React from 'react'
+import { Grid, Typography } from '@material-ui/core'
+import { Menu, MenuItem } from '../Menu'
+import Container from '../../Shared/Container'
 import Head from './Header.styles'
 import logo from '../../assets/images/marvel.jpg'
 
 const Header = () => (
   <Head data-testid="data-header">
-    <img src={logo} alt="Logo" />
+    <Container>
+      <Grid container spacing={3} direction="row" justify="flex-start" alignItems="center">
+        <Grid item xs={4}>
+          <Typography variant="h1" component="h1">
+            <img src={logo} alt="Logo" />
+          </Typography>
+        </Grid>
+        <Grid item xs={8}>
+          <Menu>
+            <MenuItem link="/hero">Home</MenuItem>
+            <MenuItem link="/busca">Busca</MenuItem>
+          </Menu>
+        </Grid>
+      </Grid>
+    </Container>
   </Head>
 )
 
