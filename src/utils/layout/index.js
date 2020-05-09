0f28be90f42c631bfec85b815351a71f1df79173
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 00:51:47 -0300

    feat: add header component in layout componet

--- src/utils/layout/index.js
+++ src/utils/layout/index.js
@@ -1,9 +1,11 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import Header from '../../Components/Header'
 import GlobalStyle from './Layout.styles'
 
 const Layout = ({ children }) => (
   <>
+    <Header />
     <div data-testid="data-layout">{children}</div>
     <GlobalStyle />
   </>
