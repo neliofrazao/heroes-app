271146d2676c67919bf589f31adbb22fb88906ed
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 10:32:05 -0300

    test: inject router in header test

--- src/Components/Header/Header.test.js
+++ src/Components/Header/Header.test.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { render } from '@testing-library/react'
+import injectRouter from '../../helpers/testHelpers'
 import Header from './index'
 
-describe('Layout()', () => {
+describe('Header()', () => {
   test('should render Header component ', () => {
-    const { getByTestId } = render(<Header />)
+    const { getByTestId } = render(injectRouter(<Header />))
     expect(getByTestId('data-header')).toBeDefined()
   })
 })
