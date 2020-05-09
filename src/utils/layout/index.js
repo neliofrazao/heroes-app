59b07ac88e9bd7b5c0849e4bc5c998f4459ed67b
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 16:19:40 -0300

    feat: add Main component in layout component

--- src/utils/layout/index.js
+++ src/utils/layout/index.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Header from '../../Components/Header'
+import Main from './Main'
 import GlobalStyle from './Layout.styles'
 
 const Layout = ({ children }) => (
   <>
     <div data-testid="data-layout">
       <Header />
-      <main>{children}</main>
+      <Main>{children}</Main>
     </div>
     <GlobalStyle />
   </>
