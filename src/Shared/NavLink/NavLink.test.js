e8cde6aa059f98ed96022a22f41241307e694d93
Nelio Frazao <neliofrazac@gmail.com>
2020-05-09 12:31:01 -0300

    test: 'should href to equal a '/hero'

---
+++ src/Shared/NavLink/NavLink.test.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import { render } from '@testing-library/react'
+import injectRouter from '../../helpers/testHelpers'
+import NavLink from './index'
+
+const BaseRender = () => (
+  <NavLink to="/hero" title="some link" data-testid="data-nav-link">
+    Some Link
+  </NavLink>
+)
+
+describe('NavLink()', () => {
+  test('should href to equal a "/hero"', () => {
+    const { getByTestId } = render(injectRouter(<BaseRender />))
+    const SUT = getByTestId('data-nav-link').getAttribute('href')
+
+    expect(SUT).toEqual('/hero')
+  })
+})
