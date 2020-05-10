c1b61059272c93bf73c700a2831ae2ab5b5fac25
Nelio Frazao <neliofrazac@gmail.com>
2020-05-10 02:07:11 -0300

    feat: map api data result and apply in Card component

--- src/Pages/HeroList/index.js
+++ src/Pages/HeroList/index.js
@@ -1,25 +1,45 @@
-import React, { useEffect, useState } from 'react'
+import React, { Fragment, useEffect, useState } from 'react'
+import { Grid } from '@material-ui/core'
+import Card from '../../Components/Card'
 import api from '../../api/characters/characters'
 
 const HeroList = () => {
-  // eslint-disable-next-line no-unused-vars
   const [characters, setCharacters] = useState([])
+  const [isLoad, setIsLoad] = useState(false)
 
   useEffect(() => {
     const getCharactersData = async () => {
+      setIsLoad(true)
       try {
         const { data } = await api.getCharacters()
         setCharacters(data)
       } catch (error) {
         setCharacters([])
       }
+      setIsLoad(false)
     }
     getCharactersData()
   }, [])
 
   return (
     <div data-testid="data-hero-list">
-      <p>Hero List</p>
+      <>
+        <Grid container direction="row" justify="flex-start" alignItems="flex-start" spacing={4}>
+          {!isLoad && characters.results ? (
+            <>
+              {characters.results.map(({ id, name, thumbnail: { path, extension } }) => (
+                <Fragment key={id}>
+                  <Grid item xs={3}>
+                    <Card imgUrl={`${path}.${extension}`} name={name} link="/link" />
+                  </Grid>
+                </Fragment>
+              ))}
+            </>
+          ) : (
+            <p>Carregando</p>
+          )}
+        </Grid>
+      </>
     </div>
   )
 }
