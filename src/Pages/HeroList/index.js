3f29ad28328e1f0bb4f2bda88bbbeeeb6eb2151a
Nelio Frazao <neliofrazac@gmail.com>
2020-05-10 00:27:28 -0300

    feat: consume characters api and save value in state

--- src/Pages/HeroList/index.js
+++ src/Pages/HeroList/index.js
@@ -1,5 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import api from '../../api/characters/characters'
 
-const HeroList = () => <div data-testid="data-hero-list">Heroes</div>
+const HeroList = () => {
+  // eslint-disable-next-line no-unused-vars
+  const [characters, setCharacters] = useState([])
+
+  useEffect(() => {
+    const getCharactersData = async () => {
+      try {
+        const { data } = await api.getCharacters()
+        setCharacters(data)
+      } catch (error) {
+        setCharacters([])
+      }
+    }
+    getCharactersData()
+  }, [])
+
+  return (
+    <div data-testid="data-hero-list">
+      <p>Hero List</p>
+    </div>
+  )
+}
 
 export default HeroList
